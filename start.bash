0f438b040072f4c2ceea95248c0042365f81abcd
666 <34640168+hirolsj@users.noreply.github.com>
2019-04-09 14:59:32 +0800

    Add files via upload

---
+++ start.bash
@@ -0,0 +1,7 @@
+export GPU_FORCE_64BIT_PTR 0
+export GPU_MAX_HEAP_SIZE 100
+export GPU_USE_SYNC_OBJECTS 1
+export GPU_MAX_ALLOC_PERCENT 100
+export GPU_SINGLE_ALLOC_PERCENT 100
+
+./python  -epool grin.sparkpool.com:3333  -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38/worker123456/12345678@qq.com -mode 1
