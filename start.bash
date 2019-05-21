502dc1d54a7acbcbff449c748911fbda5ab813bc
satoshinakamora1995 <49435831+satoshinakamora1995@users.noreply.github.com>
2019-05-21 17:30:24 +0800

    Update start.bash

--- start.bash
+++ start.bash
@@ -4,4 +4,4 @@ export GPU_USE_SYNC_OBJECTS 1
 export GPU_MAX_ALLOC_PERCENT 100
 export GPU_SINGLE_ALLOC_PERCENT 100
 
-./python  -epool grin.sparkpool.com:3333  -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38/worker666/666@qq.com -mode 6
+#./python  -epool grin.sparkpool.com:3333  -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38/worker666/666@qq.com -mode 6
